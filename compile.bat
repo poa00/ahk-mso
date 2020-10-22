934031553a9aae627f320b78d34954aa22282ae6
Thierry Dalon <tdalon.work+github@gmail.com>
2020-10-22 11:29:19 +0200

    add PTB.exe

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 ::taskkill.exe /F /IM ConNextEnhancer.exe
 ::D:\Programs\AutoHotkey\Compiler\Ahk2Exe.exe /in "ConNextEnhancer.ahk" /icon "icons\connext.ico"
 ::D:\Programs\AutoHotkey\Compiler\Ahk2Exe.exe /in "Chromy.ahk" /icon "icons\chrome.ico"
-D:\Programs\AutoHotkey\Compiler\Ahk2Exe.exe /in "Teamsy.ahk" /icon "icons\Teams.ico"
+D:\Programs\AutoHotkey\Compiler\Ahk2Exe.exe /in "PowerToolsBundler.ahk" /icon "PowerToolsBundler.ico"
 ::D:\Programs\AutoHotkey\Compiler\Ahk2Exe.exe /in "ie.ahk"
 
 
